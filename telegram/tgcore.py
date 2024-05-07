7c0b7219a3e5a77bb3070b863d6fd619f8200813
ValgrindLLVM <valgrindllvm@proton.me>
2024-05-07 19:48:35 +0300

    Add /start and /help commands

--- telegram/tgcore.py
+++ telegram/tgcore.py
@@ -2,10 +2,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from models.config import get_config
+from aiogram.types import FSInputFile
 
 BOT = Bot(
     token=get_config().bot_token,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML),
 )
+
+POSTER_IMG = FSInputFile("poster.png")
 
 dispatcher: Dispatcher = Dispatcher()
